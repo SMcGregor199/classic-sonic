1b7222c4e3322c56b8b80bcf7b6e7fb998ad3e1e
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-01-18 06:12:25 -0500

    installed prettier package and add to pre file

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from "@rollup/plugin-eslint"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),    {
+    ...eslint(),
+    enforce: 'pre',
+    apply: 'build',
+  },],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
