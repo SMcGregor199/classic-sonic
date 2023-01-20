0c5fc0f80e77e734fecde6a0c7ee252635055f75
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-01-20 06:56:53 -0500

    add contact page + learn props in components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,15 @@
 <script setup>
-	defineProps({
-		msg: {
-			type: String,
-			required: true,
-		},
-	});
+defineProps({
+	msg: String,
+	customMsg: String
+});
 </script>
 
 <template>
 	<div class="greetings">
 		<h1 class="green">{{ msg }}</h1>
+		<h1 class="green">{{ customMsg }}</h1>
+
 		<h3>
 			You’ve successfully created a project with
 			<a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
@@ -19,25 +19,26 @@
 </template>
 
 <style scoped>
-	h1 {
-		font-weight: 500;
-		font-size: 2.6rem;
-		top: -10px;
-	}
+h1 {
+	font-weight: 500;
+	font-size: 2.6rem;
+	top: -10px;
+}
 
-	h3 {
-		font-size: 1.2rem;
-	}
+h3 {
+	font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+	text-align: center;
+}
+
+@media (min-width: 1024px) {
 
 	.greetings h1,
 	.greetings h3 {
-		text-align: center;
+		text-align: left;
 	}
-
-	@media (min-width: 1024px) {
-		.greetings h1,
-		.greetings h3 {
-			text-align: left;
-		}
-	}
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+//creating an instance of vue from the vue folder
 import { createApp } from 'vue';
+//creating an instance of pinia from the pinia folder (data stores)
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
+//loading from the router folder, assumes there is an index file
 import router from './router';
 
+// This is where styles are being brought in
 import './assets/main.css';
 
 const app = createApp(App);
