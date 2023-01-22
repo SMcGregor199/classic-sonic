0ec25db75d7c7f9d289aa6c97944108f9fd60200
SMcGregor199 <shaynemcgregor1@gmail.com>
2023-01-22 12:29:01 -0500

    …

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue';
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" customMsg="This is my custom Shadow" />
+      <HelloWorld customMsg="This is my custom Shadow" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
